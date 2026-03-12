9b8d81ec771aaea1b475ede728aad584ee275cee
Jude <jude_flowsheep@163.com>
2026-03-13 00:42:52 +0800

    Add hybrid retrieval

--- app/rag/services.py
+++ app/rag/services.py
@@ -1,14 +1,16 @@
 from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters  import CharacterTextSplitter,RecursiveCharacterTextSplitter
+from langchain_text_splitters  import RecursiveCharacterTextSplitter
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaEmbeddings
-# from langchain_community.embeddings import DashScopeEmbeddings
+from langchain_classic.retrievers import BM25Retriever,EnsembleRetriever
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 
-
+embedding = OllamaEmbeddings(
+    model = 'qwen3-embedding:0.6b'
+)
 # 加载文档
 
 loader = TextLoader('/Users/yangyifan/Develop/fastapi-langchain-portfolio/中华人民共和国治安管理处罚法_20250627.txt')
@@ -21,17 +23,15 @@ documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(
     separators = ['\n\n', '\n', ' ', '。', '！', '？'],
     )
+
+
 texts = text_splitter.split_documents(documents)
 
+
+
+
+
 # 创建向量数据库
-
-embedding = OllamaEmbeddings(
-    model = 'qwen3-embedding:0.6b'
-)
-# embedding = DashScopeEmbeddings(
-#     model="text-embedding-v4"
-# )
-
 if os.path.exists('./chroma_db') or os.path.isfile('./chroma_db'):
     db =Chroma(
         persist_directory='app/rag/chroma_db',
@@ -46,8 +46,16 @@ else:
     )
     print('数据库创建成功')
 def get_retrieval_result(query):
-    retrievar = db.as_retriever()
-    return retrievar.invoke(query)
+    retrievar = db.as_retriever(
+        search_type = 'similarity',
+        search_kwargs = {
+            'k': 5
+        }
+    )
+    bm25_retriever = BM25Retriever.from_documents(texts)
+    ensemble_retriever = EnsembleRetriever(retrievers=[retrievar, bm25_retriever],weights=[0.6, 0.4])
+    return ensemble_retriever.invoke(query)
+
 
 
 
